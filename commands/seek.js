08ceefe7bd2daeb9754bd6a60e1596d16a4ca1aa
oo273825 <97148927+oo273825@users.noreply.github.com>
2023-01-19 23:35:51 +0800

    Update seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
   name: "seek",
-  description: "Seek to a position in the song",
+  description: "將當前歌曲跳至指定時間播放",
   usage: "<time s/m/h>",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
@@ -22,12 +22,12 @@ module.exports = {
     if (!player)
       return client.sendTime(
         message.channel,
-        "❌ | **Nothing is playing right now...**"
+        "❌ | **現在沒有播放的曲目...**"
       );
     if (!message.member.voice.channel)
       return client.sendTime(
         message.channel,
-        "❌ | **You must be in a voice channel to use this command!**"
+        "❌ | **你必須先加入語音頻道!**"
       );
     if (
       message.guild.me.voice.channel &&
@@ -35,18 +35,18 @@ module.exports = {
     )
       return client.sendTime(
         message.channel,
-        "❌ | **You must be in the same voice channel as me to use this command!**"
+        "❌ | **你必須和張先生在同一個語音頻道才能使用這個命令!**"
       );
     if (!player.queue.current.isSeekable)
       return client.sendTime(
         message.channel,
-        "❌ | **I'm not able to seek this song!**"
+        "❌ | **NO NO**"
       );
     let SeekTo = client.ParseHumanTime(args.join(" "));
     if (!SeekTo)
       return client.sendTime(
         message.channel,
-        `**Usage - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example - **\`${GuildDB.prefix}seek 2m 10s\``
+        `**用法 - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example - **\`${GuildDB.prefix}seek 2m 10s\``
       );
     player.seek(SeekTo * 1000);
     message.react("✅");
@@ -56,7 +56,7 @@ module.exports = {
         options: [
             {
                 name: "position",
-                description: "Enter a timestamp you want to seek to. Example - 2m 10s",
+                description: "輸入您要查找的時間戳. Example - 2m 10s",
                 value: "position",
                 type: 3,
                 required: true,
@@ -72,14 +72,14 @@ module.exports = {
                     const member = guild.members.cache.get(interaction.member.user.id);
                     let player = await client.Manager.get(interaction.guild_id);
                     
-                    if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-                    if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, "❌ | **You must be in the same voice channel as me to use this command!**");
-                    if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**");
-                    if (!player.queue.current.isSeekable) return client.sendTime(interaction, "❌ | **I'm not able to seek this song!**");
+                    if (!member.voice.channel) return client.sendTime(interaction, "❌ | **你必須先加入語音頻道!**");
+                    if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, "❌ | **你必須和張先生在同一個語音頻道才能使用這個命令!**");
+                    if (!player) return client.sendTime(interaction, "❌ | **現在沒有播放的曲目...**");
+                    if (!player.queue.current.isSeekable) return client.sendTime(interaction, "❌ | **未找到此歌曲!**");
                     let SeekTo = client.ParseHumanTime(interaction.data.options[0].value);
-                    if (!SeekTo) return client.sendTime(interaction, `**Usage - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example -** \`${GuildDB.prefix}seek 2m 10s\``);
+                    if (!SeekTo) return client.sendTime(interaction, `**用法 - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example -** \`${GuildDB.prefix}seek 2m 10s\``);
                     player.seek(SeekTo * 1000);
-                    client.sendTime(interaction, "✅ | **Successfully moved the song to **", `\`${Seekto}\``);
+                    client.sendTime(interaction, "✅ | **成功將歌曲跳至 **", `\`${Seekto}\``);
                 },
             },
         ],
